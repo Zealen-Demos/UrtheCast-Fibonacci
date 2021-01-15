3cfb727394e07de7366720aeb2b399eae93c7672
Dylan Jack <fall_back_up@hotmail.com>
2021-01-15 01:10:43 -0800

    Fibonacci function

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -6,6 +6,16 @@ namespace UrtheCast
 {
     public class Fibonacci
     {
-        
+        public static int CalculatePosition(int position, int sum, int a, int b)
+        {
+
+            Console.WriteLine("Position: " + a);
+            if (position == 0)
+            {
+                return a;
+            }
+
+            return CalculatePosition(position - 1, a + b, b, a + b);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace UrtheCast
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Fibonacci.CalculatePosition(5, 0, 0, 1));
         }
     }
 }
