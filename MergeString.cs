d1fa2ba520fda10eb54b636d8804f7fd94761e69
Dylan Jack <fall_back_up@hotmail.com>
2021-01-15 05:40:22 -0800

    Merge function

---
+++ MergeString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UrtheCast
+{
+    public class ListManipulation
+    {
+        public static List<String> Merge(List<String> original, List<String> add, List<String> remove)
+        {
+            //Concat Original and add
+            var list = original.Concat(add);
+
+            //Remove from List
+            foreach (String t in remove)
+            {
+                list = list.Where(item => item != t);
+            }
+
+            //Sort order
+            return list.OrderByDescending(item => item?.Length)
+                .ThenByDescending(item => item)
+                .Distinct()
+                .ToList();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UrtheCast
 {
@@ -6,7 +7,13 @@ namespace UrtheCast
     {
         static void Main(string[] args)
         {
+            List<String> original = new List<String>{ "one", "two", "three", "thxxx", "thaaa" };
+            List<String> add = new List<String> { "one", "two", "five", "six" };
+            List<String> remove = new List<String> { "two", "five" };
+
             Console.WriteLine(Fibonacci.CalculatePosition(5, 0, 0, 1));
+
+            ListManipulation.Merge(original, add, remove).ForEach(Console.WriteLine);
         }
     }
 }
